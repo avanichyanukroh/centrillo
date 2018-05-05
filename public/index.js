71c9850b0c4ba5af02373bdb2d0126ae10c6fbf2
Anawin Vanichyanukroh <avanichyanukroh@gmail.com>
2018-05-05 01:18:34 -0700

    completed mock design layout

--- public/index.js
+++ public/index.js
@@ -1,17 +1,22 @@
-$(document).ready(function () {
-    $("#sidebar").mCustomScrollbar({
-        theme: "minimal"
-    });
+  $('.dropdown').on('shown.bs.dropdown', function () {
+    var menu = $(this).find('.dropdown-menu');
+    var menuLeft = menu.offset().left;
+    var menuWidth = menu.outerWidth();
+    var documentWidth = $(body).outerWidth();
+    if (menuLeft + menuWidth > documentWidth) {
+      menu.offset({'left': documentWidth - menuWidth});
+    }
+  })
+};
 
-    $('#dismiss, .overlay').on('click', function () {
-        $('#sidebar').removeClass('active');
-        $('.overlay').fadeOut();
-    });
 
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').addClass('active');
-        $('.overlay').fadeIn();
-        $('.collapse.in').toggleClass('in');
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+function watchNavCollapseToggle() {
+
+    $("#navCollapse").click(function() {
+
+    $(this).toggleClass("fa-window-close");
+
     });
-});
+}
+
+$(watchNavCollapseToggle);
